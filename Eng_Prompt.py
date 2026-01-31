4a07d7a7a6d533564db1966c49dffa393e54d561
Яковлева Сардаана <svpm2016@gmail.com>
2026-01-31 16:50:48 +0700

    Загрузка программы разметки

---
+++ Eng_Prompt.py
@@ -0,0 +1,52 @@
+import spacy
+import json
+import re
+
+def process_prompts(input_file, output_file):
+    nlp = spacy.load("en_core_web_sm")
+    with open(input_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    prompts = [prompt.strip() for prompt in re.split(r'\d+\.\s*', content)[1:] if prompt.strip()] #разбивка по нумерации
+
+    corpus = []
+    for idx, prompt_text in enumerate(prompts, start=1): #разметка
+        doc = nlp(prompt_text)
+        tokens = []
+        for token in doc: #морфологическая разметка
+            lemma = token.lemma_
+            pos = token.pos_
+
+            tokens.append({
+                "word": token.text,
+                "lemma": lemma,
+                "pos": pos,
+                "morph": token.morph.to_dict(),
+                "intellectual_activity": {}
+            })
+
+        root_tokens = [token.text for token in doc if token.dep_ == "ROOT"]
+        root = root_tokens[0] if root_tokens else None
+        dependencies = []
+        for token in doc:
+            dependencies.append({
+                   "head": token.head.text,
+                   "dependent": token.text,
+                   "relation": token.dep_
+               })
+
+        corpus.append({
+            "id": idx,
+            "text": prompt_text,
+            "tokens": tokens,
+            "syntax": {
+                "root": root,
+                "dependencies": dependencies,
+            },
+            "language": "en"
+        })
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(corpus, f, ensure_ascii=False, indent=2)
+
+process_prompts("eng_prompts.txt", "en_corpus.json")
